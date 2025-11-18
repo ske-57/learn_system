c3f3397a1637fb6d7266b24a21bbb592d065a531
Arseniy Kuzenkov <senya57k@gmail.com>
2025-11-18 16:22:15 +0300

    Initial commit

---
+++ src/test/java/com/example/learn_system/LearnSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learn_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
